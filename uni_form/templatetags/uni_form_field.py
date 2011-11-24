7a77bcdb9b134afb41ab44885574cba9a741a95f
Javed Khan <javed@agiliq.com>
2011-11-24 18:02:54 +0530

    add error class to form fields

--- uni_form/templatetags/uni_form_field.py
+++ uni_form/templatetags/uni_form_field.py
@@ -17,7 +17,8 @@ def with_class(field):
     class_name = field.field.widget.__class__.__name__.lower()
     class_name = class_converter.get(class_name, class_name)
     initial_classes = field.field.widget.attrs.pop('class', '')
-    field.field.widget.attrs['class'] = "%s %s" %(initial_classes,
-            field.css_classes(extra_classes=class_name))
+    error_class = " input-error" if field.errors else ""
+    field.field.widget.attrs['class'] = "%s %s%s" %(initial_classes,
+            field.css_classes(extra_classes=class_name), error_class)
 
     return field
